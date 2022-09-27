33baead5bfa7ee8718c84ca5f18d95105b10ec1e
Denis76842 <112509983+Denis76842@users.noreply.github.com>
2022-09-27 22:12:27 +0600

    Add files via upload

---
+++ Zadacha19.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 19");
+Console.Write("Введи число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
